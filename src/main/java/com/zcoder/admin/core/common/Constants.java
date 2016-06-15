edd77a18f4372618a5dc776a970707f92e181a67
zl0422 <286434733@qq.com>
2016-06-15 16:14:43 +0800

    init

--- src/main/java/com/zcoder/admin/core/common/Constants.java
+++ src/main/java/com/zcoder/admin/core/common/Constants.java
@@ -10,6 +10,10 @@ public class Constants {
 	public final static String DEL_FLAG_VALID = "0";
 	
 	public final static String DEL_FLAG_INVALID = "1";
-	
+
+	public final static String SUCCESS_MSG = "操作成功";
+
+	public final static String ERROR_MSG = "操作失敗";
+
 
 }
